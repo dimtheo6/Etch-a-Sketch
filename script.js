e5431274fdcb8d77872290b9e65b88283b3b0039
Dimitris Theodosopoulos <155730305+dimtheo6@users.noreply.github.com>
2024-04-23 11:24:39 +0300

    created a button to change the color to rainbow

--- script.js
+++ script.js
@@ -5,39 +5,87 @@ const deleteBtn = document.querySelector('.removeBtn');
 function start(){
 
     newGrid.addEventListener('click', ()=>{
+          
+        input = parseInt(prompt('Enter a number between 1-100'));
         
-        input = prompt('Enter the size of the grid');
-        if (input <= 100) {
-            
-            calc = input * input;
-            removeGrid();
-            createGrid();
+        if ((input <= 100) && (input>0)) {
+            createGrid(input);
         }else{
-            input = prompt('Enter the size of the grid');
+            alert('Please Enter a valid number between 1-100');
 
         }
-
 
         document.querySelectorAll('.cell').forEach((item)=>{
             let mouseOver = function () {
                 this.style.background = 'green'
             }
             item.onmouseover = mouseOver;
+        })  
+        document.getElementById('blackColor').addEventListener('click',()=>{
+            let newOpacity = 0.1;
+            document.querySelectorAll('.cell').forEach((item)=>{
+                
+                let mouseOver = function (){
+                    
+                    this.style.opacity = newOpacity;
+                    newOpacity += 0.1;
+                    this.style.background = 'black'
+                }
+                item.onmouseover = mouseOver;
+               
+                
+            })
         })
-        
+        document.getElementById('rainbowBtn').addEventListener('click',()=>{
+
+            document.querySelectorAll('.cell').forEach((item)=>{
+                let mouseOver = function (){
+                    rainbowColor();
+                    this.style.background = rainbow;
+                }
+                item.onmouseover = mouseOver;
+            })
+
+        })
+
     })
 
+function darkenColor(){
+    document.querySelectorAll('.cell').forEach((item)=>{
+        let mouseOver = function () {
+            let newOpacity = 1;
+            
+            for (i=0;i<10;i++){
+                this.style.opacity = newOpacity;
+                newOpacity = newOpacity + 0.1;
+                
+            }
+        }
+        item.onmouseover = mouseOver;
+    }) 
+}
 
-function createGrid(cell){
+function rainbowColor(){
+    let red = Math.floor(Math.random()*256);
+    let blue = Math.floor(Math.random()*256);
+    let green = Math.floor(Math.random()*256);
 
+    return rainbow = "rgb("+red+","+green+","+blue+")";
+}
+
+function createGrid(size){
+
+    removeGrid();
+    const cellSize = calcSize(size);
+    const calc = size * size;
 
     for (i=0; i<calc;i++){
         cell = document.createElement('div');
         cell.classList.add('cell'); 
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
         grid.appendChild(cell);
     }
-
-
 }
 
 function removeGrid(cell){  
@@ -47,16 +95,14 @@ function removeGrid(cell){
     
 }
 
-createGrid();
-
-
-document.querySelectorAll('.cell').forEach((item)=>{
-    let mouseOver = function () {
-        this.style.background = 'green'
-    }
-    item.onmouseover = mouseOver;
-})
-
+function calcSize(size) {
+    // Calculate cell size dynamically based on grid size
+    const containerWidth = grid.offsetWidth; // Width of the grid container
+    const cellMargin = 0; // Adjust as needed
+    const totalMargins = (size - 1) * cellMargin; // Total margins between cells
+    const availableWidth = containerWidth - totalMargins; // Subtract margin space
+    return Math.floor(availableWidth / size);
+}
 
 }
 start();   
